419512a7da79eeb457156deb1303991baad278cb
veryrad <veryraddotnet@gmail.com>
2018-04-13 21:28:40 -0400

    add backup script

---
+++ backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##### bkp.sh #####
+# A tiny backup script using rdiff-backup.
+
+# Set target and backup locations
+TARGET=$HOME
+BACKUP="pi::/mnt/raid1/backup"
+
+# runs backup
+rdiff-backup \
+        -v4 \
+        --exclude $HOME'/.*' \
+        --exclude $HOME/temp \
+        --exclude $HOME/downloads \
+        --exclude $HOME/pictures/.extra \
+        $TARGET \
+        $BACKUP
