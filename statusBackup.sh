36e4b7550c3cc3d4cb4221140253b18a441e8377
veryrad <veryraddotnet@gmail.com>
2018-04-24 22:29:17 -0400

    cleanup

--- statusBackup.sh
+++ statusBackup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+##### statusBackup.sh
+# A small backup status script for rdiff-backup
+
 # define hostname of backup
 HOST="pi"
+BACKUP="pi:/mnt/raid1/backup"
 
 # test connection. If is is, return "-"
 if ping -c 1 $HOST &> /dev/null
@@ -9,7 +13,6 @@ then
         # test if rdiff-backup is running. If it is, return "*"
         if ! pgrep "rdiff-backup" > /dev/null
         then
-                BACKUP="pi:/mnt/raid1/backup"
                  # fine the time of the most recent backup
                 TIME=$(rdiff-backup -l --parsable-output pi::/mnt/raid1/backup | tail -1 | grep -o -E '[0-9]+')
                 # get the current time in Ecoch
@@ -25,14 +28,3 @@ then
 else
         echo "-"
 fi
-# define where the backup is
-    #BACKUP="pi:/mnt/raid1/backup"
-     # fine the time of the most recent backup
-    #TIME=$(rdiff-backup -l --parsable-output pi::/mnt/raid1/backup | tail -1 | grep -o -E '[0-9]+')
-    # get the current time in Ecoch
-    #NOW=$(date +%s)
-    # find time difference in seconds
-    #DIFF=$(expr $NOW - $TIME)
-    # find time difference in days
-    #DAYS=$(expr $DIFF / 24 / 3600)
-    #echo $DAYS
