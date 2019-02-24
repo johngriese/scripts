e348e00e1e2f399ad1b1ffdf056da7b092cdd7d4
John Griese <mail@johngriese.info>
2019-02-23 22:02:15 -0500

    add universalized backup script

---
+++ backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+rsync_tmbackup.sh /home/rad /var/run/media/rad/6535a580-ae33-46bf-a4e6-75a70cb8fbdb/gemini/ /var/run/media/rad/6535a580-ae33-46bf-a4e6-75a70cb8fbdb/gemini/excluded_patterns.txt
+BACKUPHOST="gemini"
+BACKUPDIR="/var/run/media/rad/6535a580-ae33-46bf-a4e6-75a70cb8fbdb"
+EXCLUDEFILE="excluded_patterns.txt"
+
+case "$HOSTNAME" in
+    gemini)
+        rsync_tmbackup.sh /home/rad "$BACKUPDIR/$HOSTNAME" "$BACKUPDIR/$HOSTNAME/$EXCLUDEFILE"
+        ;;
+    capricorn)
+        ping -q -c3 gemini > /dev/null
+        if ! [ $? -eq 0 ]
+        then
+           echo "cannot reach $BACKUPHOST; aborting" 
+           exit 1
+        fi
+        rsync_tmbackup.sh /home/rad "$BACKUPHOST:/$BACKUPDIR/$HOSTNAME" "$BACKUPHOST:/$BACKUPDIR/$HOSTNAME/$EXCLUDEFILE" 
+        ;;
+    *)
+        echo "not running from a known host; aborting"
+        exit 1
+esac
